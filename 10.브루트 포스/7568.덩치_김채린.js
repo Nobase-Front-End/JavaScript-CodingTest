a3cce67d15efb5519e09163ab912a36182653b27
chaerin.dev <chaerin.dev@gmail.com>
2022-08-13 21:34:41 +0900

    📝 브루트 포스 문제 풀이

--- 10.브루트 포스/7568.덩치_김채린.js
+++ 10.브루트 포스/7568.덩치_김채린.js
@@ -6,12 +6,17 @@ const [N, ...ARR] = require("fs")
 
 const SIZE = ARR.map((e) => e.split(" ")).map((e) => e.map((ie) => +ie));
 
-const RANK = Array.from({ length: N }, (_) => 1);
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (i === j) continue;
-    if (SIZE[i][0] < SIZE[j][0] && SIZE[i][1] < SIZE[j][1]) RANK[i]++;
+function solution(N, SIZE) {
+  // 각 사람의 순위를 저장할 배열: 모든 사람의 순위 초기값을 1로 설정
+  const RANK = Array.from({ length: N }, (_) => 1);
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (i === j) continue;
+      // 나보다 키도 크고 몸무게도 많이 나가는 사람이 있으면 나의 순위값 1 증가
+      if (SIZE[i][0] < SIZE[j][0] && SIZE[i][1] < SIZE[j][1]) RANK[i]++;
+    }
   }
+  return RANK.join(" ");
 }
 
-console.log(RANK.join(" "));
+console.log(solution(N, SIZE));
