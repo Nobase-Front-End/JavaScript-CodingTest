d4da5b9fe9e9f8501a994ad2de46bfeae17a1153
CHAERYEON-LEE <sogogijjang@naver.com>
2022-08-20 23:48:58 +0900

    정렬 문제풀이

---
+++ 11.정렬/1181.단어정렬_이채련_담당.js
@@ -0,0 +1,19 @@
+const [n, ...arr] = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+
+
+function solution(){
+  const N = input[0];
+  const words = [];
+
+  console.log(a);
+
+}
+
+solution(); 
