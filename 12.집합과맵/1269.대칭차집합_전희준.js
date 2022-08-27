1b703bf9301c9065a7ab44677834101f092e5db1
SleeplessN <gmlwns7020@naver.com>
2022-08-27 23:05:04 +0900

    3주차 집합과맵 문제풀이

---
+++ 12.집합과맵/1269.대칭차집합_전희준.js
@@ -0,0 +1,22 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+// 차집합 프로포타입 메서드
+Set.prototype.difference = function (set) {
+  const result = new Set(this);
+  for (const value of set) {
+    result.delete(value);
+  }
+  return result;
+};
+
+function solution() {
+  const setA = new Set(input[1].split(" "));
+  const setB = new Set(input[2].split(" "));
+  console.log(setA.difference(setB).size + setB.difference(setA).size);
+}
+
+solution();
